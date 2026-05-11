971cc50941a110e736dadb679e3e2aaf103783df
Dmitry Sharshukov <regulDgreate@gmail.com>
2026-05-11 07:16:33 +0300

    feat: "solution"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (typeof matrix === 'undefined') {
+    return [];
+  }
+  return matrix.flatMap((element, index) => {
+    return index === 1 || index === 3 ? [...element].reverse() : element;
+  });
 };
+
+/* module.exports = function towelSort(matrix) {
+  const newArray = [];
+  if (typeof matrix === 'undefined') {
+    return newArray;
+  }
+  matrix.forEach((element, index) => {
+    if (index === 1 || index === 3) {
+      newArray.push(...element.reverse());
+    } else {
+      newArray.push(...element);
+    }
+  });
+  return newArray;
+}; */
